b48575b30b7dbea63da22e1ef3a3e42bb7adf304
Lunarantic <lunarantic@users.noreply.github.com>
2018-03-24 19:11:19 -0400

    trial_1

--- speak_this/src/main/java/lunarantic/tts/speak_this/MainActivity.java
+++ speak_this/src/main/java/lunarantic/tts/speak_this/MainActivity.java
@@ -3,18 +3,21 @@ package lunarantic.tts.speak_this;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Locale;
+import java.io.IOException;
+import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
 
     TextToSpeech tts;
     EditText textToSpeak;
     Button speakButton;
+    Properties prop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +27,33 @@ public class MainActivity extends AppCompatActivity {
         speakButton = findViewById(R.id.button);
         textToSpeak = findViewById(R.id.editText);
 
+        prop = new Properties();
+
+        try {
+            //prop.load(new FileReader(new File("file:///assets/MessagesBundles_"+getApplicationContext().getResources().getConfiguration().locale.getLanguage()+".properties")));
+            prop.load(getResources().getAssets().open("MessagesBundles_"+getApplicationContext().getResources().getConfiguration().locale.getLanguage()+".properties"));
+        } catch (IOException e) {
+            Log.e("main", e.getMessage());
+            prop = null;
+        }
+
         tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status == 0) {
                     Toast.makeText(getApplicationContext(), "System is ready",
                             Toast.LENGTH_SHORT);
+                    if (prop == null) {
+                        textToSpeak.setText("You know New York, you need New York, you know you need unique New York");
+                    } else {
+                        textToSpeak.setText(prop.getProperty("You are ready to go"));
+                    }
                 }
             }
         });
 
-        tts.setLanguage(Locale.US);
+        //tts.setLanguage(Locale.US);
+        tts.setSpeechRate(0.75f);
 
         speakButton.setOnClickListener(new View.OnClickListener() {
             @Override
